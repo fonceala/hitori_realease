01ef81880346ff5ce342f72b62855f765066c0cb
fonceala <51325519+fonceala@users.noreply.github.com>
2020-09-15 22:54:28 +0300

    Added ComboBox to .fxml

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,8 +13,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+
+        primaryStage.setTitle("Hitori Game");
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
 
 
