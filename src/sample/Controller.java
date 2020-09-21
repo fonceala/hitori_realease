9be80700b5afb3fdae9d5383e93b01e28587a9d1
fonceala <51325519+fonceala@users.noreply.github.com>
2020-09-21 13:52:44 +0300

    Added numbers for matrix 5x5

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -12,10 +12,13 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.awt.*;
@@ -35,6 +38,7 @@ public class Controller implements Initializable {
     @FXML
     private ComboBox<String> box;
 
+    private Matrix game;
     @FXML
 
         public void handle(ActionEvent event) {
@@ -60,20 +64,38 @@ public class Controller implements Initializable {
     @FXML
     public void handleStartButton(ActionEvent event){
             int matrixSize = Integer.valueOf(box.getValue());
+            game = new Matrix();
             for(int i = 0; i < matrixSize; i++){
                 for(int j = 0; j < matrixSize; j++){
                     Rectangle rectangle = new Rectangle();
+                    int[][] matrix={};
                     rectangle.setOnMouseClicked(new EventHandler<MouseEvent>() {
                         @Override
                         public void handle(MouseEvent mouseEvent) {
                             rectangle.setFill(Color.BLACK);
                         }
                     });
+                    switch (matrixSize){
+                        case 5:
+                            matrix=game.getMatrix5();
+                            break;
+                        case 6:
+                            matrix = game.getMatrix6();
+                            break;
+                        case 7:
+                            matrix = game.getMatrix7();
+                            break;
+                    }
+                    Text text = new Text(String.valueOf(matrix[i][j]));
+                    text.setFill(Color.WHITE);
                     rectangle.setStroke(Color.BLACK);
                     rectangle.setHeight(50);
                     rectangle.setWidth(50);
                     rectangle.setFill(Color.RED);
-                    gameMatrix.add(rectangle,j,i,1,1);
+
+                    StackPane stack = new StackPane();
+                    stack.getChildren().addAll(rectangle,text);
+                    gameMatrix.add(stack,j,i,1,1);
                 }
             }
     }
--- src/sample/Matrix.java
+++ src/sample/Matrix.java
@@ -2,5 +2,46 @@ package sample;
 
 public class Matrix {
 
+    private int[][] matrix4 = {};
+    private int[][] matrix5 = {
+            {1,1,3,5,5},
+            {4,2,5,3,1},
+            {4,3,3,5,4},
+            {5,1,2,3,3},
+            {3,3,4,1,1}
+    };
+    private int[][] matrix6 = {};
+    private int[][] matrix7 = {};
 
+    public int[][] getMatrix4() {
+        return matrix4;
+    }
+
+    public void setMatrix4(int[][] matrix4) {
+        this.matrix4 = matrix4;
+    }
+
+    public int[][] getMatrix5() {
+        return matrix5;
+    }
+
+    public void setMatrix5(int[][] matrix5) {
+        this.matrix5 = matrix5;
+    }
+
+    public int[][] getMatrix6() {
+        return matrix6;
+    }
+
+    public void setMatrix6(int[][] matrix6) {
+        this.matrix6 = matrix6;
+    }
+
+    public int[][] getMatrix7() {
+        return matrix7;
+    }
+
+    public void setMatrix7(int[][] matrix7) {
+        this.matrix7 = matrix7;
+    }
 }
